e1e59c7c13818afb9b08748170a986a0442b71d0
CASIXx1 <casix.work@gmail.com>
2026-07-07 12:53:36 +0900

    Reimplement seed data in Golang

--- cmd/seeds/main.go
+++ cmd/seeds/main.go
@@ -42,4 +42,62 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	_, err = sqlDB.Exec(`
+		INSERT INTO users (username, email, password)
+		VALUES ('admin', 'admin@example.com', 'password')
+		ON CONFLICT (email) DO NOTHING;
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = sqlDB.Exec(`
+		CREATE TABLE IF NOT EXISTS projects (
+		    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+		    slug VARCHAR(255) NOT NULL UNIQUE
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = sqlDB.Exec(`
+		INSERT INTO projects (slug)
+		VALUES ('programming'),
+		('english'),
+		('design')
+		ON CONFLICT (slug) DO NOTHING;
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = sqlDB.Exec(`
+		CREATE TABLE IF NOT EXISTS tasks (
+		    title VARCHAR(255) NOT NULL,
+		    status VARCHAR(255) NOT NULL DEFAULT 'scheduled',
+		    project_id UUID NOT NULL,
+		    FOREIGN KEY (project_id) REFERENCES projects(id),
+			UNIQUE (title, project_id)
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = sqlDB.Exec(`
+		INSERT INTO tasks (title, project_id)
+		SELECT task.title, projects.id
+		FROM (
+		  VALUES
+			('Learn Go', 'programming'),
+			('Learn English', 'english'),
+			('Learn Design', 'design')
+		) AS task(title, project_slug)
+		JOIN projects ON projects.slug = task.project_slug;
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
