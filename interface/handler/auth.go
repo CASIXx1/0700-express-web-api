180cd0f23ec62b18b0bffa0a68bf0963d3614fa6
CASIXx1 <casix.work@gmail.com>
2026-07-27 23:51:56 +0900

    Refactor: merge `request` and `response` structs into `auth.go` for improved cohesion and reduce file fragmentation

--- interface/handler/auth.go
+++ interface/handler/auth.go
@@ -11,6 +11,31 @@ type Handler struct {
 	authUsecase *usecase.AuthUsecase
 }
 
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type SignUpRequest struct {
+	Username             string `json:"username"`
+	Email                string `json:"email"`
+	EmailConfirmation    string `json:"email_confirmation"`
+	Password             string `json:"password"`
+	PasswordConfirmation string `json:"password_confirmation"`
+}
+
+type LoginResponse struct {
+	UUID         string `json:"uuid"`
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
+type SignUpResponse struct {
+	UUID         string `json:"uuid"`
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
 func NewHandler(authUsecase *usecase.AuthUsecase) *Handler {
 	return &Handler{
 		authUsecase: authUsecase,
