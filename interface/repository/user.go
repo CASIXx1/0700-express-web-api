9443c3db49fa84c0316ef98c42184badb8f24b7a
CASIXx1 <casix.work@gmail.com>
2026-08-04 23:07:41 +0900

    Add `FindUserByID` method to `UserRepository` with UUID parsing support

--- interface/repository/user.go
+++ interface/repository/user.go
@@ -4,6 +4,8 @@ import (
 	"0700-express-web-api/ent"
 	entuser "0700-express-web-api/ent/user"
 	"context"
+
+	"github.com/google/uuid"
 )
 
 type UserRepository struct {
@@ -21,6 +23,15 @@ func (repository *UserRepository) FindUserByEmail(ctx context.Context, email str
 		Query().
 		Where(entuser.EmailEQ(email)).
 		First(ctx)
+}
+
+func (repository *UserRepository) FindUserByID(ctx context.Context, userID string) (*ent.User, error) {
+	id, err := uuid.Parse(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return repository.dbClient.User.Get(ctx, id)
 }
 
 func (repository *UserRepository) CreateUser(ctx context.Context, username, email, hashedPassword string) (*ent.User, error) {
