f32a1bd665fdb93c55db185d3b6f7eb9682f3122
CASIXx1 <casix.work@gmail.com>
2026-08-27 13:33:48 +0900

    Refactor route setup: rename subrouter to `authRequired` for clarity

--- main.go
+++ main.go
@@ -57,11 +57,11 @@ func main() {
 
 	r.Use(middleware.RequestLog)
 
-	s := r.NewRoute().Subrouter()
-	s.Use(middleware.Auth(tokenService, userRepository))
-	s.HandleFunc("/users/me", userHandler.Me).Methods(http.MethodGet)
-	s.HandleFunc("/users/projects", projectHandler.FindProjects).Methods(http.MethodGet)
-	s.HandleFunc("/users/projects/{slug}", projectHandler.FindProjectBySlug).Methods(http.MethodGet)
+	authRequired := r.NewRoute().Subrouter()
+	authRequired.Use(middleware.Auth(tokenService, userRepository))
+	authRequired.HandleFunc("/users/me", userHandler.Me).Methods(http.MethodGet)
+	authRequired.HandleFunc("/users/projects", projectHandler.FindProjects).Methods(http.MethodGet)
+	authRequired.HandleFunc("/users/projects/{slug}", projectHandler.FindProjectBySlug).Methods(http.MethodGet)
 
 	addr := ":8080"
 	log.Printf("server listening on %s\n", addr)
