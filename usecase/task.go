1ed94796570f9a8776f055521ea6229cfefc9fa3
CASIXx1 <casix.work@gmail.com>
2026-09-03 22:13:42 +0900

    Add `TaskUsecase`: implement `FindTasks` with pagination and filtering logic

---
+++ usecase/task.go
@@ -0,0 +1,49 @@
+package usecase
+
+import (
+	"0700-express-web-api/ent"
+	"context"
+)
+
+type TaskUsecase struct {
+	taskRepository TaskRepository
+}
+
+type TaskListResult struct {
+	Tasks    []*ent.Task
+	PageInfo PageInfo
+}
+
+type TaskRepository interface {
+	FindTasks(ctx context.Context, userID string, statuses []string, limit int, offset int) ([]*ent.Task, error)
+	CountTasks(ctx context.Context, userID string, statuses []string) (int, error)
+}
+
+func NewTaskUsecase(taskRepository TaskRepository) *TaskUsecase {
+	return &TaskUsecase{
+		taskRepository: taskRepository,
+	}
+}
+
+func (usecase *TaskUsecase) FindTasks(ctx context.Context, userID string, statuses []string, page int, limit int) (*TaskListResult, error) {
+	offset := (page - 1) * limit
+
+	totalCount, err := usecase.taskRepository.CountTasks(ctx, userID, statuses)
+	if err != nil {
+		return nil, err
+	}
+
+	tasks, err := usecase.taskRepository.FindTasks(ctx, userID, statuses, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TaskListResult{
+		Tasks: tasks,
+		PageInfo: PageInfo{
+			TotalCount:  totalCount,
+			HasPrevious: page > 1,
+			HasNext:     offset+limit < totalCount,
+		},
+	}, nil
+}
