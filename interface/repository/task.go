07cc34fd130830afee3158df18d6829c02ad5bba
CASIXx1 <casix.work@gmail.com>
2026-09-12 12:10:06 +0900

    Revert "Refactor `CreateTask` to use transactional operations, ensuring atomicity and improved error handling; unwrap entities before returning."
    This reverts commit 9657080d6ff2763f3a4078e933c6ddcf2080a0c1.

--- interface/repository/task.go
+++ interface/repository/task.go
@@ -45,19 +45,12 @@ func NewTaskRepository(client *ent.Client) *TaskRepository {
 }
 
 func (repository *TaskRepository) CreateTask(ctx context.Context, userID uuid.UUID, input CreateTaskInput) (*ent.Task, error) {
-	tx, err := repository.client.Tx(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	project, err := tx.Project.
+	project, err := repository.client.Project.
 		Query().
 		Where(entProject.ID(input.ProjectID)).
 		Where(entProject.UserID(userID)).
 		Only(ctx)
 	if err != nil {
-		tx.Rollback()
-
 		if ent.IsNotFound(err) {
 			return nil, ErrNotFound
 		}
@@ -65,7 +58,7 @@ func (repository *TaskRepository) CreateTask(ctx context.Context, userID uuid.UU
 		return nil, err
 	}
 
-	task, err := tx.Task.
+	task, err := repository.client.Task.
 		Create().
 		SetTitle(input.Title).
 		SetDescription(input.Description).
@@ -78,17 +71,9 @@ func (repository *TaskRepository) CreateTask(ctx context.Context, userID uuid.UU
 		SetProjectID(project.ID).
 		Save(ctx)
 	if err != nil {
-		tx.Rollback()
 		return nil, err
 	}
 
-	err = tx.Commit()
-	if err != nil {
-		return nil, err
-	}
-
-	task = task.Unwrap()
-	project = project.Unwrap()
 	task.Edges.Project = project
 
 	return task, nil
